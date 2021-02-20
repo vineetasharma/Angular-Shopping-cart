4d9c56b46883e155b33d53eb8cac1a668fcd2fec
Vineeta Sharma <vineetasharma18791@gmail.com>
2021-02-20 23:28:24 +0530

    basic components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
+import { ItemComponent } from './item/item.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
-const routes:Routes=[
+const routes: Routes = [
   {
-  path:'',component:CategoryComponent
-  }
-  ];
-  
-  @NgModule({
+    path: '', component: CategoryComponent
+  },
+  {
+    path: 'item', component: ItemComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
+  },
+];
+
+@NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { CategoryComponent } from './category/category.component';
+import { LoginComponent } from './login/login.component';
+import { ItemComponent } from './item/item.component';
+import { CartComponent } from './cart/cart.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    CategoryComponent
+    CategoryComponent,
+    LoginComponent,
+    ItemComponent,
+    CartComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
